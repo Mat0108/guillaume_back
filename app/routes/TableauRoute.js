fbc10031c9c2eeb8a8cbc9a5a1401ffea3721f4e
Matthieu B <mesjeux0108@gmail.com>
2025-12-07 11:31:30 +0100

    fix multer

--- app/routes/TableauRoute.js
+++ app/routes/TableauRoute.js
@@ -1,15 +1,15 @@
-// const multer = require("multer")
+const multer = require("multer");
 module.exports = (server,corsConfig) => {
     const TableauController = require("../controllers/TableauxController");
     const cors = require('cors');
 
+    const upload = multer({ storage: multer.memoryStorage() });
     server.post("/tableau/getAll", cors(corsConfig), TableauController.getAllTableaux);
     server.get("/tableau/getById/:tableauId", cors(corsConfig),TableauController.getTableauById);
     server.post("/tableau/getByExpo",cors(corsConfig),TableauController.getTableauxByExpo)
-    // server.post("/tableau/create",upload.single("file"),cors(corsConfig),TableauController.createTableau);
+    server.post("/tableau/create",upload.single("file"),cors(corsConfig),TableauController.createTableau);
 
-    // const upload = multer({ storage: multer.memoryStorage() });
-    // server.post("/tableau/addMultipleImage/",upload.array("files",10),cors(corsConfig),TableauController.addMultipleImage)
+    server.post("/tableau/addMultipleImage/",upload.array("files",10),cors(corsConfig),TableauController.addMultipleImage)
     server.get("/tableau/:tableauId/getImage",cors(corsConfig),TableauController.getImage)
     
     server.post("/tableau/:tableauId/addExpo",cors(corsConfig),TableauController.addExpo)
@@ -17,6 +17,6 @@ module.exports = (server,corsConfig) => {
    
     server.post("/tableau/:tableauId/update",cors(corsConfig),TableauController.updateTableau)
     server.post("/tableau/:tableauId/rotate",cors(corsConfig),TableauController.rotateTableau)
-    // server.post("/tableau/:tableauId/updateTableau",upload.single("file"),cors(corsConfig),TableauController.updateTableauImage)
+    server.post("/tableau/:tableauId/updateTableau",upload.single("file"),cors(corsConfig),TableauController.updateTableauImage)
    
 }
